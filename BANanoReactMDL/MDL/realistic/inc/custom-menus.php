8195248af2eb013c30b85dde1e694313e86bb0f3
Mashy <mbanga.anele@gmail.com>
2019-09-09 03:21:05 +0200

    Updates

--- BANanoReactMDL/MDL/realistic/inc/custom-menus.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * @package realistic
- */
-
-if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
-
-require_once ( get_template_directory() . '/inc/classes/class-realistic-mobile-menu-walker.php' );
